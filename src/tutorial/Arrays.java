8c9072fe2cd426ecfeb92a52dcfa1108ed8b9c5f
OlegFrozzzze <ji900@yandex.ru>
2017-11-15 05:25:28 +0300

    Added minimum values ​​and keyboard input

--- src/tutorial/Arrays.java
+++ src/tutorial/Arrays.java
@@ -1,7 +1,11 @@
 package tutorial;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Arrays {
-    public static void arraysHello(){
+    public static void arraysMAX() {
         byte a = Byte.MAX_VALUE;
         int b = Integer.MAX_VALUE;
         short c = Short.MAX_VALUE;
@@ -9,13 +13,40 @@ public class Arrays {
         double e = Double.MAX_VALUE;
         float f = Float.MAX_VALUE;
 
-        Object[] arrays = new Object[]{a,b,c,d,e,f};
-        for(int i = 0; i<=5;i++){
+        Object[] arrays = new Object[]{a, b, c, d, e, f};
+        for (int i = 0; i <= 5; i++) {
             System.out.println(arrays[i]);
         }
     }
 
-    public static void main(String[] args) {
-        arraysHello();
+    public static void arraysMIN() {
+        byte a = Byte.MIN_VALUE;
+        int b = Integer.MIN_VALUE;
+        short c = Short.MIN_VALUE;
+        long d = Long.MIN_VALUE;
+        double e = Double.MIN_VALUE;
+        float f = Float.MIN_VALUE;
+
+        Object[] arrays = new Object[]{a, b, c, d, e, f};
+        for (int i = 0; i <= 5; i++) {
+            System.out.println(arrays[i]);
+        }
+    }
+
+    public static void arraysHelloTwo() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Вибирите: MIN или MAX");
+        String value = reader.readLine();
+        if (value.equals("MIN")){
+            arraysMIN();
+
+        } else {
+            if (value.equals("MAX")) {
+                arraysMAX();
+            } else {
+                System.out.println("Ошибка ввода введите заново");
+                arraysHelloTwo();
+            }
+        }
     }
 }
--- src/tutorial/Main.java
+++ src/tutorial/Main.java
@@ -1,8 +1,11 @@
 package tutorial;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world");
+        Arrays.arraysHelloTwo();
     }
 }
