d81dc6178e9ada47358e5ccdfabd8cb712bdce3d
OlegFrozzzze <ji900@yandex.ru>
2017-11-17 01:35:07 +0300

    Implementation of Caesar encryption

---
+++ src/tutorial/NewLv.java
@@ -0,0 +1,10 @@
+package tutorial;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// А давайка реализуем метот шифрование с ключём (абракадабра + сколько то симвоов = настоящие слово)
+public class NewLv {
+
+}
